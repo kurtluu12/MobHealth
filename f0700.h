615c276d57690dfd5c101bdcf6bcb03911da15ba
kurtluu12 <kluu007@ucr.edu>
2018-09-17 14:15:47 -0700

    Added finishing touches and description

--- f0700.h
+++ f0700.h
@@ -1,7 +1,8 @@
 #include<stdio.h>
+#ifndef __f0700_h__
+#define __f0700_h__
 
-// eliminate uneccesary pointer arithmetic, variables, loop, calculation
-signed int f0700(int a1, int *a2, int a3, int *a4)
+signed int  f0700(int a1, int *a2, int a3, int *a4)
 {
    // Pre-Condition:
    //    a1: possible input value = [64, 128, 256]
@@ -17,14 +18,11 @@ signed int f0700(int a1, int *a2, int a3, int *a4)
    //    a3: value shouldn't change
    //    a4: value can change
 
-    int t1, t2, t3, t4, v5, v6, v7, v9, v11, v12, v13, v14, v15, o1, a2a;
-
+    int t1, t2, t3, t4, v5, v6, v9, v11, v12, o1, a2a;
+ 
     v5 = a1;
     v6 = 0;
-    v7 = a2[a1];
     v9 = -1;
-    v14 = a2[a1 - 1];
-    v15 = v7;
     o1 = 0;
     a2a = 0;
 
@@ -33,10 +31,10 @@ signed int f0700(int a1, int *a2, int a3, int *a4)
         do
         {
             v11 = a2[v6];
-            v6++;           // Separate incremententation from a2[v6++]
+            v6++;           
             v12 = a2[v6]; 
-            
-            t1 = v9 * (v11 - v12);      // Do branch arithmetic outside for better readability
+
+            t1 = v9 * (v11 - v12);      
             if (t1 < a3)    
             {
                 do
@@ -56,28 +54,16 @@ signed int f0700(int a1, int *a2, int a3, int *a4)
                     v5 = a1;
             }
             v9 = -v9;
-            a2a++;      // separate a2a++ from a2a++ + 1
+            a2a++;      
             o1 = a2a + 1; 
         } while (v6 < v5);
     }
 
-    if(o1 % 2) // add {}
+    if(o1 % 2) 
     { 
         --o1;
     }
     return o1;
 }
 
-int main() {
-
-    //int arr[1408]; // a2
-    //int arr2[116]; // a4
-
-    int arr[10] = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4};
-    int arr2[10] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-
-    signed int x = f0700( 2, arr, 6, arr2);
-    printf("%d\n", x);
-    return 0;
-}
-
+#endif
